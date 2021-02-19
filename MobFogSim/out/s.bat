05fe7ac15a50589e67d19d76ca2ae900fd2e5261
Best System <Best System@DESKTOP-CVVARQF>
2021-02-19 23:17:15 +0330

    e.g. first commit

---
+++ MobFogSim/out/s.bat
@@ -0,0 +1,2 @@
+del /q "E:\Programs\OWN\Projects\Fog\MobFogSim-master\MobFogSim-master\out\livea\*"
+FOR /D %%p IN ("E:\Programs\OWN\Projects\Fog\MobFogSim-master\MobFogSim-master\out\livea\*.*") DO rmdir "%%p" /s /q
