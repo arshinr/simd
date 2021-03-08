5dfe1ed96bf03be38d6b18be34a3c9544206611a
abednejad <abednejad@gmail.com>
2021-03-08 21:14:33 +0330

    delete project

--- MobFogSim/out/s.bat
+++
@@ -1,2 +0,0 @@
-del /q "E:\Programs\OWN\Projects\Fog\MobFogSim-master\MobFogSim-master\out\livea\*"
-FOR /D %%p IN ("E:\Programs\OWN\Projects\Fog\MobFogSim-master\MobFogSim-master\out\livea\*.*") DO rmdir "%%p" /s /q
