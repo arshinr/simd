60e10072292b835c3d741627247715e86aa33411
Arshin <arshinr@yahoo.com>
2021-04-18 20:43:27 -0400

    Update script0110cold.sh

--- MobFogSim/script0110cold.sh
+++ MobFogSim/script0110cold.sh
@@ -16,7 +16,7 @@ do
 		mkdir $STNumber
 
 #		java -Xmx30g -Dfile.encoding=UTF-8 -classpath bin:jars/cloudsim-3.0.3-sources.jar:jars/cloudsim-3.0.3.jar:jars/cloudsim-examples-3.0.3-sources.jar:jars/cloudsim-examples-3.0.3.jar:jars/commons-math3-3.5/commons-math3-3.5.jar:jars/guava-18.0.jar:jars/json-simple-1.1.1.jar:jars/junit.jar:jars/org.hamcrest.core_1.3.0.v201303031735.jar org.fog.vmmobile.AppExample 1 290538 0 0 $STNumber 11 0 0 0 61
-		java -Xmx30g -Dfile.encoding=UTF-8 -classpath ".\bin;.\jars\cloudsim-3.0.3.jar;.\jars\cloudsim-3.0.3-sources.jar;.\jars\cloudsim-examples-3.0.3.jar;.\jars\cloudsim-examples-3.0.3-sources.jar;.\jars\guava-18.0.jar;.\jars\json-simple-1.1.1.jar;.\jars\junit.jar;.\jars\org.hamcrest.core_1.3.0.v201303031735.jar;.\jars\commons-math3-3.5\commons-math3-3.5.jar" org.fog.vmmobile.AppExample 1 290538 0 0 $STNumber 11 0 0 0 61
+		java -Xmx30g -Dfile.encoding=UTF-8 -classpath bin:jars/cloudsim-3.0.3-sources.jar:jars/cloudsim-3.0.3.jar:jars/cloudsim-examples-3.0.3-sources.jar:jars/cloudsim-examples-3.0.3.jar:jars/commons-math3-3.5/commons-math3-3.5.jar:jars/guava-18.0.jar:jars/json-simple-1.1.1.jar:jars/junit.jar:jars/org.hamcrest.core_1.3.0.v201303031735.jar org.fog.vmmobile.AppExample 1 290538 0 0 $STNumber 11 0 0 0 61 1000 2439
 
 		mv *.txt $STNumber
 		mv $STNumber out/cold/
